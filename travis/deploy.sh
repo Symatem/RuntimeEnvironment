4ef46ba415c62090bbe3a8cf448c26b24cfd114e
Markus Felten <Markus.Felten@gmx.de>
2016-10-01 13:01:10 +0200

    chore(travis): deploy wip

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ARCHIVE_NAME=Symatem_$(uname -s)_$(uname -p).tar
+PRIVATE_KEY=travis/travis_id_rsa
+
+echo "uploading $ARCHIVE_NAME ..."
+
+chmod 400 $PRIVATE_KEY
 
 rm build/$ARCHIVE_NAME
 (cd ./build;tar cvf $ARCHIVE_NAME *)
 
-scp -i travis/travis_id_rsa ./build/$ARCHIVE_NAME mfelten_de@mfelten.de:/home/mfelten_de/docroot/Symatem/
+scp -i $PRIVATE_KEY ./build/$ARCHIVE_NAME mfelten_de@mfelten.de:/home/mfelten_de/docroot/Symatem/
