e7e649cc0ababffdfd82d5240ecebdec6f2f2584
Alexander Meißner <AlexanderMeissner@gmx.net>
2017-05-21 07:39:39 +0200

    Refactored Set and MetaSet to use the same code

--- DataStructures/Set.hpp
+++ DataStructures/Set.hpp
@@ -1,20 +1,21 @@
 #include <DataStructures/Heap.hpp>
 
-template<typename KeyType, typename ValueType = VoidType, typename _ParentType = BitVectorContainer>
-struct Set : public PairVector<KeyType, ValueType, _ParentType> {
+template<typename _Super, typename KeyType, typename ValueType, typename _ParentType>
+struct SetTemplate : public _Super {
+    typedef _Super Super;
     typedef _ParentType ParentType;
-    typedef PairVector<KeyType, ValueType, ParentType> Super;
     typedef typename Super::ElementType ElementType;
 
-    Set(ParentType& _parent, NativeNaturalType _childIndex = 0) :Super(_parent, _childIndex) {}
+    SetTemplate(ParentType& _parent, NativeNaturalType _childIndex = 0) :Super(_parent, _childIndex) {}
     usingRemappedMethod(insertElement)
     usingRemappedMethod(eraseElementByKey)
 
     bool findKey(KeyType key, NativeNaturalType& at) {
-        at = binarySearch<NativeNaturalType>(0, Super::getElementCount(), [&](NativeNaturalType at) {
+        NativeNaturalType elementCount = Super::getElementCount();
+        at = binarySearch<NativeNaturalType>(0, elementCount, [&](NativeNaturalType at) {
             return Super::getKeyAt(at) < key;
         });
-        return (at < Super::getElementCount() && Super::getKeyAt(at) == key);
+        return (at < elementCount && Super::getKeyAt(at) == key);
     }
 
     bool setKeyAt(NativeNaturalType at, KeyType key) {
@@ -27,4 +28,10 @@ struct Set : public PairVector<KeyType, ValueType, _ParentType> {
         Super::setKeyAt(newAt, key);
         return true;
     }
+};
+
+template<typename KeyType, typename ValueType = VoidType, typename _ParentType = BitVectorContainer>
+struct Set : public SetTemplate<PairVector<KeyType, ValueType, _ParentType>, KeyType, ValueType, _ParentType> {
+    typedef SetTemplate<PairVector<KeyType, ValueType, _ParentType>, KeyType, ValueType, _ParentType> Super;
+    using Super::Super;
 };
